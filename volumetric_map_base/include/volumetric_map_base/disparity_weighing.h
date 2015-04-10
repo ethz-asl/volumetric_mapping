450e5800d2d6f745af36208b33b21185ac78307f
Helen Oleynikova <helenoleynikova@gmail.com>
2015-04-10 13:37:09 +0200

    Formatting.

--- volumetric_map_base/include/volumetric_map_base/disparity_weighing.h
+++ volumetric_map_base/include/volumetric_map_base/disparity_weighing.h
@@ -8,26 +8,27 @@ namespace volumetric_mapping {
 class DisparityWeighing : public PointWeighing {
  public:
   DisparityWeighing(double b, double f, double c_x, double c_y, double delta_d)
-    : b_(b), f_(f), c_x_(c_x), c_y_(c_y), delta_d_(delta_d) {}
+      : b_(b), f_(f), c_x_(c_x), c_y_(c_y), delta_d_(delta_d) {}
   virtual ~DisparityWeighing() {}
 
   virtual double computeWeightForDisparity(unsigned int u, unsigned int v,
                                            double d) const {
-    double z = b_*f_/d;
-    double x = (u - c_x_)/f_;
-    double y = (v - c_y_)/f_;
+    double z = b_ * f_ / d;
+    double x = (u - c_x_) / f_;
+    double y = (v - c_y_) / f_;
 
     return computeWeightForPoint(x, y, z);
   }
 
   virtual double computeWeightForPoint(double x, double y, double z) const {
-    // From "Autonomous Visual Mapping and Exploration With a Micro Aerial Vehicle"
+    // From "Autonomous Visual Mapping and Exploration With a Micro Aerial
+    // Vehicle"
     // Lionel Heng, Dominik Honegger, Gim Hee Lee, Lorenz Meier,
     // Petri Tanskanen, Friedrich Fraundorfer, Marc Pollefeys
     Eigen::Vector3d point(x, y, z);
     double r_p = point.norm();
-    double sigma = r_p*r_p/(b_*f_)*delta_d_;
-    double w = 1.0/(sigma + 1.0);
+    double sigma = r_p * r_p / (b_ * f_) * delta_d_;
+    double w = 1.0 / (sigma + 1.0);
     return w;
   }
 
